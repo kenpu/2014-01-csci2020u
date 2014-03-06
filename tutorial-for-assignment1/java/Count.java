c7b5a3ec6fe4c7196a214680e17e61802ddf0cd5
Ken Pu <ken.pu.ca@gmail.com>
2014-03-06 11:58:57 -0500

    Counting using Java, map and comparators

--- tutorial-for-assignment1/java/Count.java
+++ tutorial-for-assignment1/java/Count.java
@@ -2,6 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Count {
+    static class ByValue implements Comparator<Map.Entry<String, Integer>> {
+        public int compare(Map.Entry<String, Integer> a, Map.Entry<String, Integer> b) {
+            // compare counts in reverse order
+            return -1 * Integer.compare(a.getValue(), b.getValue());
+        }
+    }
     public static void main(String[] args) throws Exception {
         String filename = args[0];
         Map<String, Integer> counts = new TreeMap<String, Integer>();
@@ -24,6 +30,15 @@ public class Count {
         }
         reader.close();
 
+        // get all the entries into a vector
+        Vector<Map.Entry<String, Integer>> entries = new Vector<Map.Entry<String, Integer>>();
+        entries.addAll(counts.entrySet());
+        Collections.sort(entries, new ByValue());
+
         System.out.printf("There are %d distinct words.\n", counts.size());
+        for(int i=0; i < 100; i ++) {
+            Map.Entry<String, Integer> e = entries.get(i);
+            System.out.printf("\"%s\" %d\n", e.getKey(), e.getValue());
+        }
     }
 }
